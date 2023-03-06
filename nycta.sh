06f276f2f80c47a21c638a5b6d22a2143208f545
Javier Orfo <javierorfo@protonmail.com>
2023-03-05 22:22:26 -0300

    Update and rename install.sh to nycta.sh

--- nycta.sh
+++ nycta.sh
@@ -2,37 +2,38 @@
 # Author: Javier Orfo
 
 echo -e "\033[0;37m"
-echo -e " _       __ _   "
-echo -e "| | ___ / _| |_ "
-echo -e "| |/ _ \ |_| __|"
-echo -e "| |  __/  _| |_ "
-echo -e "|_|\___|_|  \__|"
+echo -e " _   _            _        "
+echo -e "| \ | |_   _  ___| |_ __ _ "
+echo -e "|  \| | | | |/ __| __/ _` |"
+echo -e "| |\  | |_| | (__| || (_| |"
+echo -e "|_| \_|\__, |\___|\__\__,_|"
+echo -e "       |___/               "
 echo -e "\033[0m"
 
 # Required
-echo "left ==> Checking packages needed..."
+echo "nycta ==> Checking packages needed..."
 sudo pacman -S --needed dmenu pamixer pulseaudio-alsa feh alacritty ttf-ubuntu-mono-nerd polybar
 yay -S --needed leftwm lux
 
 # Function to copy Leftwm
 copy_leftwm() {
-    echo "left ==> Checking and copying Leftwm folders..."
+    echo "nycta ==> Checking and copying Leftwm folders..."
 
     rm -drf ~/.config/leftwm
     cp -dr leftwm ~/.config
 
-    echo "left ==> Done! Log out and start Leftwm!"
+    echo "nycta ==> Done! Log out and start Leftwm!"
 }
 
 # Keyboard LATAM config
 if [ ! -e /etc/X11/xorg.conf.d/00-keyboard.conf ]; then
     while true; do
-        echo -e "left ==> Do you want to set 00-keyboard.conf LATAM configuration? [y/n]"
+        echo -e "nycta ==> Do you want to set 00-keyboard.conf LATAM configuration? [y/n]"
         read
         case $REPLY in 
-            [Yy] ) echo "left ==> Copying keyboard LATAM configuration...";
+            [Yy] ) echo "nycta ==> Copying keyboard LATAM configuration...";
                    sudo cp xorg/00-keyboard.conf /etc/X11/xorg.conf.d/;
-                   echo "left ==> 00-keyboard.conf copied!";
+                   echo "nycta ==> 00-keyboard.conf copied!";
                 break;;
             [Nn] ) 
                         break;;
@@ -44,16 +45,16 @@ fi
 # Validate existing Leftwm configuration
 if [ -d ~/.config/leftwm ]; then
         while true; do
-            echo -e "\033[0;33mleft ==> Leftwm configuration already exists in your home directory. "
-            echo -e "\033[0;33mleft ==> This action will delete it. Do you want to continue? [y/n]\033[0m"
+            echo -e "\033[0;33mnycta ==> Leftwm configuration already exists in your home directory. "
+            echo -e "\033[0;33mnycta ==> This action will delete it. Do you want to continue? [y/n]\033[0m"
             read
 
             case $REPLY in 
                 [Yy] ) copy_leftwm;
                     break;;
-                [Nn] ) echo "left ==> process canceled!";
+                [Nn] ) echo "nycta ==> process canceled!";
                     exit;;
-                * ) echo -e "\033[0;31mleft ==> Invalid response. Please, try again\033[0m";;
+                * ) echo -e "\033[0;31mnycta ==> Invalid response. Please, try again\033[0m";;
             esac
         done
     else
